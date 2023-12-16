2964517eb694d60391dcc139b42f5fa3ce406232
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-16 11:46:57 -0700

    Complete split string exercise

--- codewars_split_strings.mjs
+++ codewars_split_strings.mjs
@@ -1,8 +1,16 @@
 function solution(str){
 
-    
-   for(const [char, i] of Array.from(str).entries()){
-
-
+   const arr = [];
+   for(let i = 0; i < str.length; i+=2){
+      arr.push(str.slice(i,i+2).length == 2 ? str.slice(i,i+2) : str.slice(i,i+2) + '_');
    }
+   return arr;
 }
+
+// console.log('abcdefg'.slice(0,2));
+// console.log('abcdefg'.slice(2,4));
+// console.log('abcdefg'.slice(4,6));
+// console.log('abcdefg'.slice(6,8));
+// console.log('abcdefg'.slice(6,8).length == 2 ? 'abcdefg'.slice(6,8) : 'abcdefg'.slice(6,8) + '_');
+
+console.log(solution('abcdefghijkl'))
