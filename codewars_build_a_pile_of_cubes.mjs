3131646eafd6593d7462a9fc3956d88121bc4393
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-16 12:32:42 -0700

    Complete build a pile of cubes exercise

---
+++ codewars_build_a_pile_of_cubes.mjs
@@ -0,0 +1,18 @@
+function findNb(m) {
+    let total = 0;
+    let currN = 1;
+    while(total < m){
+        
+        total += (currN ** 3);
+        
+        if (total == m){
+            return currN;
+        } else if (total > m) {
+            return (-1);
+        }
+
+        currN += 1;
+    }
+}
+
+console.log(findNb(135440716410000));
