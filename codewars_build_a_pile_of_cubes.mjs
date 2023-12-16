6c5feb78fecd12ecadcd0aeb8fc127c4c49e052b
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-16 12:40:36 -0700

    Polish build a pile of cubes exercise

--- codewars_build_a_pile_of_cubes.mjs
+++ codewars_build_a_pile_of_cubes.mjs
@@ -4,15 +4,13 @@ function findNb(m) {
     while(total < m){
         
         total += (currN ** 3);
-        
         if (total == m){
             return currN;
         } else if (total > m) {
             return (-1);
         }
-
         currN += 1;
     }
 }
 
-console.log(findNb(135440716410000));
+console.log(findNb(4183059834009));
