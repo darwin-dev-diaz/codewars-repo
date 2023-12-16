d92d196b4d660d132a90e0e95afceeb20c0b4ffa
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-15 19:33:35 -0700

    Study the top response of rot13

--- codewars_rot13.mjs
+++ codewars_rot13.mjs
@@ -17,3 +17,18 @@ function rot13(message) {
 console.log(rot13("mC=;E:.Ri}^8G!24"));
 // console.log(rot13("Uryyb, ubj ner lbh?"));
 
+
+// what is happening here on this top response?
+function rot13_1(message) {
+  // here he's defining two alphabets, one regular and one converted with rot13
+  // its interesting here that he doesn't use the ascii table instead he just uses these two strings
+  const a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const b = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+  // the regex gi tag means that it will select all of the alphabet chars irrespective of the case
+  //  the func c => b[a.indexOf(c)]
+  // is first finding the index of the current letter in the normal alphabet list
+  // then it is pass that index into the b string which is already shifted by 13 characters. so if you pass "n" you will get its index is the a string which is 13, and then you pass that index into the b string. 
+  // you will get a because a is at index 13
+
+  return message.replace(/[a-z]/gi, c => b[a.indexOf(c)])
+}
