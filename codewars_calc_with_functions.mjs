aad7ecace539d40b443cd0ed566b94ddebb8ae4f
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-16 12:14:19 -0700

    Study best practice for calc with functions exercise

--- codewars_calc_with_functions.mjs
+++ codewars_calc_with_functions.mjs
@@ -1,3 +1,24 @@
+// Best practice
+// here the number is saying that if a function is passed into it, return the function with the number as the parameter
+// if the number doesn't get a function passed into it, then just return the number
+function zero1(fn) {return fn ? fn(0) : 0}
+function one1(fn) {return fn ? fn(1) : 1}
+function two1(fn) {return fn ? fn(2) : 2}
+function three1(fn) {return fn ? fn(3) : 3}
+function four1(fn) {return fn ? fn(4) : 4}
+function five1(fn) {return fn ? fn(5) : 5}
+function six1(fn) {return fn ? fn(6) : 6}
+function seven1(fn) {return fn ? fn(7) : 7}
+function eight1(fn) {return fn ? fn(8) : 8}
+function nine1(fn) {return fn ? fn(9) : 9}
+
+// the arithmatic operators take in a number and return a function. the function says "add v with n" we provide n and the number we pass the operator into will provide v
+function plus1(n) {return function(v) {return v + n}}
+function minus1(n) {return function(v) {return v - n}}
+function times1(n) {return function(v) {return v * n}}
+function dividedBy1(n) {return function(v) {return v / n}}
+// BEST PRACTICE END
+
 function decideOp(num, arr){
     switch (arr[0]){
         case '+':
