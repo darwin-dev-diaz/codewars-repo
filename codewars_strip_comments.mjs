3b5a69be4d50e1d9c1ed6070e16a329b93d30dcf
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-17 12:36:26 -0700

    Study best practices for strip comments exercise

--- codewars_strip_comments.mjs
+++ codewars_strip_comments.mjs
@@ -1,14 +1,20 @@
 function solution(input, markers) {
+    // this regular expression gets the text after the marker identifier
     const expression = `\\s*(${markers[0] == "$" ? '\\$' : markers[0]}|${markers[1] == "$" ? '\\$' : markers[1]})(.)+`;
     const re = new RegExp(expression, 'g');
 
     input = input.replace(re, "");
     return input.trim();
 }
-console.log(solution("a #b\nc\nd $e f g", ['#', '$' ]));
-// let commentOne1 = '#'
-// let commentTwo1 = '!'
-// let expression1 = commentTwo ? `\s(${commentOne}|${commentTwo})(.)+` : `(${commentOne})(.)+`;
-// let re = new RegExp(expression, 'g');
-
-// console.log("apples, pears # and bananas\ngrapes\nbananas !apples".replace(re, ""));
+// BEST PRACTICES START
+// ONE
+function solution(input, markers) {
+    // split input by lines into an array
+    return input.split('\n').map(
+        // map that array to a function that takes a line as input
+      line => markers.reduce(
+        (line, marker) => line.split(marker)[0].trim(), line
+      )
+    ).join('\n')
+  }
+// BEST PRACTICES END
