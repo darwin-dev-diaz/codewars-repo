be0282f6fd8843613164d0516ff78cc3335467de
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-17 13:02:48 -0700

    Complete build tower exercise

--- codewars_build_tower.mjs
+++ codewars_build_tower.mjs
@@ -1,12 +1,18 @@
 function towerBuilder(nFloors) {
-    const arr = [];
-    let str = "8".repeat(nFloors);
-    for(nFloors; nFloors > 0; nFloors--){
-        arr.push(str);
-        str.replace(/f/,' ');
+    const outputArr = []
+    const tempArr = Array.from(' '.repeat((2*nFloors)-1));
+    const midWay = Math.floor(tempArr.length / 2);
+    tempArr[midWay] = '*';
 
-    }
+    for(let i = 1; i <= nFloors; i++){
+        outputArr.push(tempArr.join(""));
+        tempArr[Math.floor(tempArr.length/2) - i] = '*';
+        tempArr[Math.floor(tempArr.length/2) + i] = '*';
+    
+      }
+    
+    return outputArr;
+
   }
-
-  console.log('*'.repeat(8));
-  console.log('test');
+  
+  console.log(towerBuilder(1));
