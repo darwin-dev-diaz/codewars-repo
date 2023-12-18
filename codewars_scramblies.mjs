4b1b505a13af3e5c7bcf60eb5d2f3b5f59ec3ec2
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-18 11:10:10 -0700

    Fail scramble exercise again

--- codewars_scramblies.mjs
+++ codewars_scramblies.mjs
@@ -9,3 +9,15 @@ function scramble(str1, str2) {
   }
   
 console.log(scramble('fdsrejavascriptwqefdfdsafd', 'javascript'));
+
+function scramble1(str1, str2) {
+    const str1Arr = Array.from(str1);
+    const str2Arr = Array.from(str2);
+    for(let i = 0; i < str2Arr.length; i++){
+        let currCharIndex = str1Arr.indexOf(str2Arr[i]);
+        if (currCharIndex == -1){return false;}
+        str1Arr.splice(currCharIndex,1);   
+    }
+    return true;
+}
+console.log(scramble('scriptjva', 'javascript'));
