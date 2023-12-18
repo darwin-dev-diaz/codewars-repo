c0d05d369b31f1db9131c06fcda8e6a9ccb9a29a
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-18 15:38:19 -0700

    Study best practice for scramble exercise

--- codewars_scramblies.mjs
+++ codewars_scramblies.mjs
@@ -46,3 +46,37 @@ function scramble(str1, str2) {
 }
 
 console.log(scramble('javascript', 'javascriptt'));
+
+// Learning from code warriors
+function scramble1(str1, str2) {
+    // define a charcount object that will store the number of char occurrences
+    const charCount = {};
+  
+    // Count the occurrences of characters in str1
+    // i didn't know you could iterate over a string like this... it would not have been needed for me to convert the string into an arr
+    for (let char of str1) {
+        // this line of code is saying that if charCount[char] doesn't exist then set charCount[char] to 1
+        // if it does exist, then simply add one to char count
+      charCount[char] = (charCount[char] || 0) + 1;
+    }
+  
+    // Check if characters in str2 are present in str1 with sufficient count
+    for (let char of str2) {
+        // if the previously defined charCount doesn't contain occurrences of the current character, then automatically just return false.
+      if (!charCount[char]) {
+        return false;
+      }
+
+    //   if the charCount does contain the current letter, then remove one from it's count.
+    // this serves to account for the potential multiple characters in str2
+    // if we decrement too much, the charCount will eventually get to zero and if it does, the above if statement will make the function return false
+      charCount[char]--;
+    }
+  
+    return true;
+  }
+
+//   what i've learned from this exercise is that
+// - you can iterate over the characters of a string with for(const char of string)
+// - its faster to write your own methods than to rely on the built in javascript functions
+// (why is that true?)
