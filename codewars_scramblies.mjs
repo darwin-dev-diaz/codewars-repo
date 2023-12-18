a24df1121432469d5e9ea529344bcf7231efb660
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-18 15:06:16 -0700

    Revert "Complete scramblies exercise"
    This reverts commit f13669b1936ce196d786cbe9ff17f13db9a33918.

--- codewars_scramblies.mjs
+++ codewars_scramblies.mjs
@@ -1,46 +1,23 @@
-// function scramble(str1, str2) {
-//     let str1Arr = Array.from(str1);
-//     const boolArr = Array.from(str2).map(function(char){
-//         const bool = str1Arr.indexOf(char) !== -1 ? true : false;
-//         str1Arr.splice(str1Arr.indexOf(char),1);
-//         return bool;
-//     });
-//     return boolArr.indexOf(false) == -1 ? true : false;
-//   }
-  
-// console.log(scramble('fdsrejavascriptwqefdfdsafd', 'javascript'));
-
-// function scramble1(str1, str2) {
-//     const str1Arr = Array.from(str1);
-//     const str2Arr = Array.from(str2);
-//     for(let i = 0; i < str2Arr.length; i++){
-//         let currCharIndex = str1Arr.indexOf(str2Arr[i]);
-//         if (currCharIndex == -1){return false;}
-//         str1Arr.splice(currCharIndex,1);   
-//     }
-//     return true;
-// }
-
-
 function scramble(str1, str2) {
-    const obj1 = {};
-    const obj2 = {};
-    const str2SetArr = Array.from(new Set(str2));
+    let str1Arr = Array.from(str1);
+    const boolArr = Array.from(str2).map(function(char){
+        const bool = str1Arr.indexOf(char) !== -1 ? true : false;
+        str1Arr.splice(str1Arr.indexOf(char),1);
+        return bool;
+    });
+    return boolArr.indexOf(false) == -1 ? true : false;
+  }
+  
+console.log(scramble('fdsrejavascriptwqefdfdsafd', 'javascript'));
 
-    // populate the objects the characters counts
-    Array.from(str1).forEach((char)=> obj1[char] ? obj1[char] += 1 : obj1[char] = 1);
-    Array.from(str2).forEach((char)=> obj2[char] ? obj2[char] += 1 : obj2[char] = 1);
-    
-    // determine if the relevant character counts for obj1 are greater or equal to the counts for obj2
-    for(let i = 0; i < str2SetArr.length; i++){
-        if(!obj1[str2SetArr[i]] || (obj1[str2SetArr[i]] < obj2[str2SetArr[i]])){
-            return false;
-        }
+function scramble1(str1, str2) {
+    const str1Arr = Array.from(str1);
+    const str2Arr = Array.from(str2);
+    for(let i = 0; i < str2Arr.length; i++){
+        let currCharIndex = str1Arr.indexOf(str2Arr[i]);
+        if (currCharIndex == -1){return false;}
+        str1Arr.splice(currCharIndex,1);   
     }
-
     return true;
-    // what happens if it just doenst contain the letter?
-    
 }
-
-console.log(scramble('javascript','javascriipt'));
+console.log(scramble('scriptjva', 'javascript'));
