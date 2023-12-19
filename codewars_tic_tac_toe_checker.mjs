ef921830ef01af7489b4c384e8d7d5c819856c17
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-19 12:41:26 -0700

    Continue work on tic tac toe exercise

--- codewars_tic_tac_toe_checker.mjs
+++ codewars_tic_tac_toe_checker.mjs
@@ -1,11 +1,57 @@
 function isSolved(board) {
-    for(const [x, row] of board.entries()){
-        for(const [y, column] of row.entries()){
-            console.log([x,y],column);
-        }
-    }
+    const checkHorizontal = [[]]
   }
 
-isSolved([[0,0,1],
-          [0,1,2],
-          [2,1,0]]);
+
+function horizontalCheck(arr, row, symbol){
+    for(let i = 0; i < 3; i++){
+        if(arr[row][i] !== symbol){
+            return false;
+        }
+    }
+    return true;
+}
+function verticalCheck(arr, column, symbol){
+    for(let i = 0; i < 3; i++){
+        if(arr[i][column] !== symbol){
+            return false;
+        }
+    }
+    return true;
+}
+function diagonalCheck(arr, startColumn, symbol){
+    let dir = startColumn > 0 ? -1 : 1;
+    for(let i = 0; i < 3; i++){
+        if(arr[0+i][startColumn+(i*dir)] !== symbol){
+            return false;
+        }
+    }
+    return true;
+}
+
+const arr = [[0,1,0],
+            [0,1,0],
+            [0,1,0]]
+const symbol = 1;
+const checkHorizontal = [[0,0],[1,0],[2,0]];
+const checkVeritcal = [[0,0],[0,1],[0,2]];
+const checkDiagnol = [[0,0],[0,2]];
+
+// for(const coords of checkHorizontal){
+//     if(horizontalCheck(arr, coords[0], symbol)){
+//         console.log(`${symbol} matched horizontally at ${coords}`);
+//         break;
+//     }
+// }
+for(const coords of checkVeritcal){
+    if(verticalCheck(arr, coords[1], symbol)){
+        console.log(`${symbol} matched vertically at ${coords}`);
+        break;
+    }
+}
+// for(const coords of checkHorizontal){
+//     if(horizontalCheck(arr, coords[0], symbol)){
+//         console.log(`${symbol} matched horizontally at ${coords}`);
+//         break;
+//     }
+// }
