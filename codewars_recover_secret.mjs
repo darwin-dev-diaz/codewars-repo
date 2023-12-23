28c7c4b4bc00c6a3d46fa33f3175ba621b3d1f68
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-23 11:15:57 -0700

    Work on recover secret exercise

--- codewars_recover_secret.mjs
+++ codewars_recover_secret.mjs
@@ -1,11 +1,60 @@
-var recoverSecret = function (triplets) {
-  let buildingList = triplets.splice(0, 1);
-  let selectedList = getSelectedList(triplets,buildingList);
-  positionChar(selectedList,buildingList);
-  console.log(buildingList);
+function recoverSecret(triplets) {
+  // find the number of letters in the secret word
+  const numberOfLetters = [...new Set(triplets.join("").replaceAll(",", ""))]
+    .length;
+  // create a building list from the the first entry of triplets. remove first entry
+  const buildingList = triplets.splice(0, 1)[0];
+  // get a selected list for all unique letters
+  for (let i = 0; i < numberOfLetters; i++) {
+    
+  }
+  
+  // search for another list in triplets that starts with the same letter as buildingList[0]. put that list into a selectedList variable
+  let selectedList = setSelectedList(buildingList, triplets);
 
-};
-let triplets1 = [
+  // find the dissimilar character (between buildingList and selectedList)
+  let dissimilarChar = selectedList.filter(
+    (char) => buildingList.indexOf(char) == -1
+  )[0];
+  // place the dissimilarChar at the correct location in buildingList
+  buildingList.splice(selectedList.indexOf(dissimilarChar), 0, dissimilarChar);
+
+  return buildingList;
+}
+
+function setSelectedList(buildingList, triplets) {
+  // case where there is a list inside of triplets where the first char is equal the the first char of buildingList
+  for (const [i, list] of triplets.entries()) {
+    if (list[0] == buildingList[0]) {
+      return triplets.splice(i, i + 1)[0];
+    }
+  }
+  // case where there isn't a list inside of triplets where the fist chat is the the same as the first char of buildingList
+  for (const [i, list] of triplets.entries()) {
+    if (list[1] == buildingList[0]) {
+      return triplets.splice(i, i + 1)[0];
+    }
+  }
+}
+
+function recoverSecret1(triplets) {
+  const numberOfLetters = [...new Set(triplets.join("").replaceAll(",", ""))]
+    .length;
+  const buildingList = triplets.splice(0, 1)[0];
+  for (let i = 0; i < numberOfLetters; i++) {
+    let selectedList = setSelectedList(buildingList, triplets);
+    let dissimilarChar = selectedList.filter(
+      (char) => buildingList.indexOf(char) == -1
+    )[0];
+    buildingList.splice(selectedList.indexOf(dissimilarChar), 0, dissimilarChar);
+    console.log(buildingList);
+  }
+
+  return buildingList;
+}
+
+
+const triplets1 = [
   ["t", "u", "p"],
   ["w", "h", "i"],
   ["t", "s", "u"],
@@ -14,39 +63,17 @@ let triplets1 = [
   ["t", "i", "s"],
   ["w", "h", "s"],
 ];
-recoverSecret(triplets1);
-const totalChars = [...new Set(triplets1.join("").replaceAll(",", ""))].length;
 
-let buildingList = [];
-// add first triple in order. remove that triple from original list
-buildingList = buildingList.concat(triplets1[0]);
-triplets1.splice(0, 1);
+// console.log(recoverSecret1(triplets1));
 
-// search to see if any of first triplets (triplets1[0][x]) match up with buildingList[0] and select that list
-function getSelectedList(tripletArr,buildingList) {
-  for (const [i, list] of tripletArr.entries()) {
-    if (list[0] == buildingList[0]) {
-      tripletArr.splice(i, i + 1);
-      return list;
-    }
-  }
-}
+let buildingList = [ 't', 'i', 's', 'u', 'p' ];
+let triplets = [
+  ["w", "h", "i"],
+  ["a", "t", "s"],
+  ["h", "a", "p"],
+  ["w", "h", "s"],
+];
 
-// positions the new characters from selectedList in buildingList correctly
-function positionChar(selectedList,buildingList) {
-  let dissimilarValue = selectedList.filter(
-    (currentValue) => !buildingList.includes(currentValue)
-  )[0];
+console.log(setSelectedList(buildingList, triplets))
 
-  let letterToLeft = selectedList[selectedList.indexOf(dissimilarValue) - 1]
-    ? selectedList[selectedList.indexOf(dissimilarValue) - 1]
-    : false;
-
-  if (letterToLeft) {
-    let index = selectedList.indexOf(letterToLeft);
-    buildingList.splice(index + 1, 0, dissimilarValue);
-  } else {
-    buildingList.splice(0, 0, dissimilarValue);
-  }
-}
-
+// current problem is that in my function loop, selectedList is set to undefined for some reason. 
