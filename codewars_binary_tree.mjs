b9958c2641124bb1891c70d5c94b97a172f9f795
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-27 11:49:54 -0700

    Complete binary tree exercise with recursion

--- codewars_binary_tree.mjs
+++ codewars_binary_tree.mjs
@@ -1,62 +1,66 @@
-class Node { 
-    constructor(value, left = null, right = null) {
-      this.value = value;
-      this.left  = left;
-      this.right = right;
-    }
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
   }
-
-  const treeOne =
-      new Node(2,
-        new Node(8,
-          new Node(1,
-            new Node(21)),
-          new Node(3,
-            null,
-            new Node(23))
-        ),
-        new Node(9,
-          new Node(4,
-            new Node(24)),
-          new Node(5,
-            new Node(25),
-            new Node(25))
-        )
-		);  
-
-function treeByLevels (rootNode) {
-    if(!rootNode) return [];
-
-
 }
 
-function childrenInList(rootNode){
-    let arr = [];
-    if(rootNode.left) arr.push(rootNode.left);
-    if(rootNode.right) arr.push(rootNode.right);
-    return arr;
+const treeOne = new Node(
+  2,
+  new Node(8, new Node(1, new Node(21)), new Node(3, null, new Node(23))),
+  new Node(
+    9,
+    new Node(4, new Node(24)),
+    new Node(5, new Node(25), new Node(25))
+  )
+);
+
+const treeTwo = new Node(2);
+
+function placeChildrenInList(rootNode) {
+  let arr = [];
+  if (rootNode.left) arr.push(rootNode.left);
+  if (rootNode.right) arr.push(rootNode.right);
+  return arr;
+}
+function getValue(rootNode) {
+  return rootNode.value;
+}
+function treeByLevels(rootNodes) {
+  if (!rootNodes || rootNodes.length == 0) return [];
+
+  let arr = [];
+  if(Array.isArray(rootNodes)){
+    rootNodes = rootNodes.map((node) => placeChildrenInList(node)).flat();
+  } else {
+    arr = arr.concat(rootNodes);
+    rootNodes = placeChildrenInList(rootNodes);
+  }
+  arr = arr.concat(rootNodes).map((node) => getValue(node));
+  return arr.concat(treeByLevels(rootNodes));
 }
 
-function getValue(rootNode){
-    return rootNode.value;
-}
-
+console.log(treeByLevels(null));
+console.log(treeByLevels(treeOne));
 
 // MY BRAIN IS HURTING
 let arr = [];
 //level one to two
-let nodeList = [];
-nodeList = childrenInList(treeOne).flat();
-console.log(nodeList);
-arr = arr.concat(nodeList.map(node=>getValue(node)));
+// let rootNodes = treeOne;
+// rootNodes = Array.isArray(rootNodes) ? rootNodes.map((node=>placeChildrenInList(node))).flat() : placeChildrenInList(treeOne).flat(); // call function here?
+// arr = arr.concat(rootNodes.map(node=>getValue(node))); // return here
 
-//level two to three
-nodeList = nodeList.map((node=>childrenInList(node))).flat();
-arr = arr.concat(nodeList.map(node=>getValue(node)));
-// console.log(nodeList);
+// // //level two to three
+// rootNodes = Array.isArray(rootNodes) ? rootNodes.map((node=>placeChildrenInList(node))).flat() : placeChildrenInList(treeOne).flat(); // call function here?
+// arr = arr.concat(rootNodes.map(node=>getValue(node)));
+// // return arr.concat(treeByLevels(rootNodes))
+// rootNodes = Array.isArray(rootNodes) ? rootNodes.map((node=>placeChildrenInList(node))).flat() : placeChildrenInList(treeOne).flat();
+// console.log(rootNodes); // call function here?
+// arr = arr.concat(rootNodes.map(node=>getValue(node)));
 
-//level three to four (This WORKS)
-nodeList = nodeList.map((node=>childrenInList(node))).flat();
-arr = arr.concat(nodeList.map(node=>getValue(node)));
-console.log(arr);
-
+let rootNodes = treeTwo;
+rootNodes = Array.isArray(rootNodes)
+  ? rootNodes.map((node) => placeChildrenInList(node)).flat()
+  : placeChildrenInList(rootNodes).flat(); // call function here?
+arr = arr.concat(rootNodes.map((node) => getValue(node))); // return here
