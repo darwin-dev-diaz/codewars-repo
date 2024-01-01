494712f563545e19bffb18a0c6269a9a2307b141
Darwin Diaz <darwin.diaz.ut@gmail.com>
2023-12-31 18:52:12 -0700

    Complete range extraction exercise

--- codewars_range_extraction.mjs
+++ codewars_range_extraction.mjs
@@ -1,28 +1,30 @@
-function solution(list){
-    // TODO: complete solution 
-   }
-function sumOfConsecutiveInts(firstTerm, lastTerm){
-    const numberOfTerms = lastTerm - firstTerm + 1;
-    return (numberOfTerms * (firstTerm + lastTerm))/2;
-}
-function rangeExtraction(arr){
-    let returnArr = arr.splice(0,1);
-    let arrCopy = arr;
-    let actualSum = returnArr[0];
-
-    for(const [i, num] of arrCopy.entries()){
-        let expectedSum = sumOfConsecutiveInts(returnArr[0],returnArr[0]+i+1);
-        actualSum += num;
-        if(expectedSum!==actualSum) break;
-        returnArr.push(num);
-    }
-
-    if(returnArr.length < 3) return returnArr[0];
-    return `${returnArr[0]}-${returnArr[returnArr.length-1]}`;
+function solution(list) {
+  let outputArr = [];
+  while (list.length > 0) {
+    outputArr.push(rangeExtraction(list));
+  }
+  return outputArr.join();
 }
 
-// i have a function that returns the nearest range from a list
+function sumOfConsecutiveInts(firstTerm, lastTerm) {
+  return ((lastTerm - firstTerm + 1) * (firstTerm + lastTerm)) / 2;
+}
 
-let testArr = [1,12,3,4,5,6,8];
-console.log(rangeExtraction(testArr)); //['1-8'] expected
-console.log(testArr);
+function rangeExtraction(arr) {
+  let returnArr = arr.splice(0, 1);
+  let actualSum = returnArr[0];
+  let expectedSum = returnArr[0];
+
+  for (const [i, num] of arr.entries()) {
+    expectedSum = sumOfConsecutiveInts(returnArr[0], returnArr[0] + i + 1);
+    actualSum += num;
+    if (expectedSum !== actualSum) break;
+    returnArr.push(num);
+  }
+
+  if (returnArr.length < 3) return returnArr[0];
+
+  // Removes range from original Array
+  arr.splice(0, arr.indexOf(returnArr[returnArr.length - 1]) + 1);
+  return `${returnArr[0]}-${returnArr[returnArr.length - 1]}`;
+}
